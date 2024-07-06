1a34e5f46c5267f76b40c74ffb35089413b655a8
Matt Woodward <woodwardmatt@gmail.com>
2024-07-06 11:29:51 +0930

    (keys) Adding ability to selectively generate EDCSA / ED25519 key types
    Signed-off-by: Matt Woodward <woodwardmatt@gmail.com>

--- modules/keys.js
+++ modules/keys.js
@@ -4,24 +4,40 @@ export class Keys{
 
     constructor(){};
 
-    static async generateKeys(){
+    static async generateKeys(isECDSA = true){
 
         //Generate a 24-word mnemonic
         const newMnemonic = await Mnemonic.generate();
     
-        //Create new keys
-        const newAccountPrivateKey = await newMnemonic.toEcdsaPrivateKey();
+        //Create new keys (ECDSA by default)
+        const newAccountPrivateKey = (isECDSA) ? await newMnemonic.toStandardECDSAsecp256k1PrivateKey() : await newMnemonic.toStandardEd25519PrivateKey();
         const newAccountPublicKey = newAccountPrivateKey.publicKey;
     
-        //Output keys
-        return {
-            phrase: newMnemonic,
-            private: newAccountPrivateKey,
-            public: newAccountPublicKey,
-            EVMprivate: '0x' + newAccountPrivateKey.toStringRaw(),
-            EVMpublic: '0x' + newAccountPublicKey.toStringRaw(),
-            EVMAddress: newAccountPublicKey.toEthereumAddress()
-        };
+        //Output keys (type specific)
+        if (isECDSA) {
+
+            //EVM Compatible ECDSA Output keys
+            return {
+                phrase: newMnemonic,
+                private: newAccountPrivateKey,
+                public: newAccountPublicKey,
+                EVMprivate: '0x' + newAccountPrivateKey.toStringRaw(),
+                EVMpublic: '0x' + newAccountPublicKey.toStringRaw(),
+                EVMAddress: newAccountPublicKey.toEvmAddress()
+            };
+
+        }else{
+
+            //Non-EVM Compatible ED25519 Output keys
+            return {
+                phrase: newMnemonic,
+                private: newAccountPrivateKey,
+                public: newAccountPublicKey
+            };
+
+        }
+
+
     }
 
 }
